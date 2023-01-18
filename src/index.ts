e35eedebf02461329cb5230c81787fdfa49a0b4a
Scott Sanford <ssanford@mediafly.com>
2023-01-18 11:48:24 -0600

    function to handle each line

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,31 @@ function processFileStream(filePath: string): void {
 		input: fileStream
 	})
 
-	let startLine = 1
-	rl.on('line', (line) => {
-		console.log(`${startLine++} -`, line)
+	const regex = /([\w\s]+)\s(\d+)\s*,\s*([\w\s]+)\s(\d+)/g
+	rl.on('line', (line: string) => {
+		const [awayTeam, homeTeam] = line.split(',')
+
+		const team1 = getTeamNameAndScore(awayTeam)
+		const team2 = getTeamNameAndScore(homeTeam)
+		console.log('1', team1)
+		console.log('2', team2)
+		console.log('======================================')
 	})
+}
+
+interface Team {
+	name: string
+	score: number
+}
+
+// TODO: Handle if there is no string length
+// TODO: Move to a separate file
+// TODO: Write unit tests
+function getTeamNameAndScore(team: string): Team {
+	const name = team.substring(0, team.length - 1)
+	const score = parseInt(team.substring(team.length - 1))
+
+	return { name, score }
 }
 
 processFileStream(filePath)
