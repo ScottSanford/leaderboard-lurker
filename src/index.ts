b337b3499dafeaa34ccce0369661c3958a4dfa79
Scott Sanford <ssanford@mediafly.com>
2023-01-18 09:41:34 -0600

    read text file from a file stream

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Does it work?')
+import { createReadStream } from 'fs'
+import path from 'path'
+import readline from 'readline'
+
+const filePath = path.join(__dirname, '..', 'input', 'sample-input.txt')
+
+function processFileStream(filePath: string): void {
+	const fileStream = createReadStream(filePath, 'utf-8')
+
+	const rl = readline.createInterface({
+		input: fileStream
+	})
+
+	let startLine = 1
+	rl.on('line', (line) => {
+		console.log(`${startLine++} -`, line)
+	})
+}
+
+processFileStream(filePath)
